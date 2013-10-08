3bb4f95b0f547e95d05d90f3e0309b940e4d419c
Iwani Khalid <iwanikhalid@gmail.com>
2013-10-08 14:44:27 +0800

    Basic theme setup

--- innobio-wptheme/footer.php
+++ innobio-wptheme/footer.php
@@ -1,13 +1,20 @@
-		<div id="footer">
-			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
 		</div>
-
+		<div id="footer">&copy; 2002-<?php echo date("Y"); ?> Inno Bio Diagnostics Sdn. Bhd. All Rights Reserved. </div>
 	</div>
+<!--[if IE]></div><![endif]--> 
 
 	<?php wp_footer(); ?>
 	
 	<!-- Don't forget analytics -->
-	
-</body>
 
+  <!-- Javascript at the bottom for fast page loading -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+	<script src="<?php bloginfo('stylesheet_url'); ?>/js/slides.min.jquery.js"></script>
+	<script src="<?php bloginfo('stylesheet_url'); ?>/js/fx.js"></script>
+	
+  <!--[if lt IE 7 ]>
+    <script src="<?php bloginfo('stylesheet_url'); ?>/js/dd_belatedpng.js?v=1"></script>
+  <![endif]-->
+  
+</body>
 </html>
--- innobio-wptheme/header.php
+++ innobio-wptheme/header.php
@@ -32,9 +32,16 @@
 		   ?>
 	</title>
 	
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favico.ico" type="image/x-icon" />
 	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/adxmenu.css" type="text/css" media="screen" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+	<!--[if lt IE 7 ]> 
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/ie6.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/ADxMenu.js"></script>
+	<![endif]-->
 	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
@@ -45,11 +52,77 @@
 </head>
 
 <body <?php body_class(); ?>>
-	
-	<div id="page-wrap">
-
+<!--[if IE]><div id="IEroot"><![endif]--> 
+	<div class="wrap">
 		<div id="header">
-			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-			<div class="description"><?php bloginfo('description'); ?></div>
+			<div class="clearfix">
+				<h1 class="floatl logo"><a href="<?php echo get_option('home'); ?>/"><span>Innobio Ventures</span></a></h1>
+				<img class="floatl mosti" src="images/mosti.gif" alt="" />
+				<div class="floatl" id="nav">
+					<ul class="menu adxm">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">About Us</a>
+							<ul>
+								<li><a href="#">Board of Directors</a></li>
+								<li><a href="#">Management Team</a></li>
+								<li><a href="#">Organization Structure</a></li>
+							</ul>						
+						</li>
+						<li><a href="#">News &amp; Events</a>
+							<ul>
+								<li><a href="#">News</a></li>
+								<li><a href="#">Events</a></li>
+								<li><a href="#">Press Release</a></li>
+							</ul>						
+						</li>
+						<li><a href="#">Career</a>
+							<ul>
+								<li><a href="#">Jobs Listing</a></li>
+								<li><a href="#">Deposit Resume</a></li>
+							</ul>						
+						</li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="page-banner">
+				<img src="images/pgbanner-home.jpg" alt="" />
+				<p class="page-quote">We Drive and Advance Bio Business Ventures for Nation's Economic Growth</p>
+			</div>
 		</div>
 
+		<div id="banner">
+			<div id="slideshow">
+				<div id="slides">
+					<div class="slides_container">
+						<div class="slide">
+							<img src="images/slide1.jpg" />
+							<div class="caption">
+								<h1>Perfect Solutions</h1>
+								<p>INNO Biologics offers complete services from upstream to downstream operation for the development of your therapeutic proteins </p>
+								<a href="">Explore Our Technology &raquo;</a>
+							</div>
+						</div>
+						<div class="slide">
+							<a href="http://www.google.com"><img src="images/slide2.jpg" /></a>
+							<div class="caption">
+								<h1>Center of Excellence</h1>
+								<p>Inno Bio Diagnostics is the center of excellence for R&amp;D of stem cell based technology for cell based diagnostics, cell based therapy and regenerative medicine. </p>
+								<a href="">Explore Our Technology &raquo;</a>
+							</div>
+						</div>
+						<div class="slide">
+							<img src="images/slide3.jpg" />
+							<div class="caption">
+								<h1>Great Location</h1>
+								<p>Bio Innovation Center (BIC) is the location of choice for bio-technology companies. We aspire to be your partner for bio-tech R&D and manufacturing. </p>
+								<a href="">Explore Our Technology &raquo;</a>
+							</div>
+						</div>
+					</div>
+					<a href="#" class="prev"><img src="images/arrow-prev.png" alt="Arrow Prev"></a>
+					<a href="#" class="next"><img src="images/arrow-next.png" alt="Arrow Next"></a>
+				</div>	
+			</div>		
+		</div>
+		<div id="content">
